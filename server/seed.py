abbeff9910dda3455a3a707e355b771f7dfdaf6b
WeddyNkatha265 <weddynkatha265@gmail.com>
2024-10-01 15:06:19 +0300

    Updates flask sqlalchemy seed

--- server/seed.py
+++ server/seed.py
@@ -5,6 +5,12 @@ from app import app
 from models import db, Pet
 
 with app.app_context():
+    
+    # Create and initialize a faker generator
+    fake = Faker()
+
+    # Delete all rows in the "pets" table
+    Pet.query.delete()
 
     # Create an empty list
     pets = []
@@ -13,6 +19,7 @@ with app.app_context():
     pets.append(Pet(name = "Fido", species = "Dog"))
     pets.append(Pet(name = "Whiskers", species = "Cat"))
     pets.append(Pet(name = "Hermie", species = "Hamster"))
+    pets.append(Pet(name = "Slither", species = "Snake"))
 
     # Insert each Pet in the list into the database table
     db.session.add_all(pets)
